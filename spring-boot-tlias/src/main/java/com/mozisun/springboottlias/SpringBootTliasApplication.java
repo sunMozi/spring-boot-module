50cdf51b9e0a731a9be155cf28de5b954560e67e
mozi <2138844072@qq.com>
2025-05-11 10:01:15 +0800

    spring boot tlias

---
+++ spring-boot-tlias/src/main/java/com/mozisun/springboottlias/SpringBootTliasApplication.java
@@ -0,0 +1,13 @@
+package com.mozisun.springboottlias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTliasApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootTliasApplication.class, args);
+  }
+
+}
---
+++ spring-boot-tlias/src/test/java/com/mozisun/springboottlias/SpringBootTliasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozisun.springboottlias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTliasApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
