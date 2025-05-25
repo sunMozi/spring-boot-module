9267ab1b2d4d762c1b45c0e581c1b8fcbee71a8d
mozi <2138844072@qq.com>
2025-05-25 15:17:33 +0800

    springboot-constant

---
+++ spring-boot-constant/src/main/java/com/mozisun/springbootconstant/SpringBootConstantApplication.java
@@ -0,0 +1,13 @@
+package com.mozisun.springbootconstant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConstantApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootConstantApplication.class, args);
+  }
+
+}
---
+++ spring-boot-constant/src/test/java/com/mozisun/springbootconstant/SpringBootConstantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozisun.springbootconstant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConstantApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
