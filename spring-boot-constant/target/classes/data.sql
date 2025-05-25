7f9cff977b8ee9d5271ef1948141cd1abdbf3640
mozi <2138844072@qq.com>
2025-05-25 15:32:32 +0800

    springboot-constant 常量 表 的 使用

---
+++ spring-boot-constant/target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO t_constant (category_code, code, name, sort_order, status)
+VALUES ('USER_STATUS', '0', '禁用', 0, 1),
+       ('USER_STATUS', '1', '启用', 1, 1),
+       ('GENDER', 'M', '男', 1, 1),
+       ('GENDER', 'F', '女', 2, 1);
